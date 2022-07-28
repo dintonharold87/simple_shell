55a0734cbf398db82ebaf5cf7ca5a16bacb3a523
DonAine <30406256+DonAine@users.noreply.github.com>
2022-07-28 02:28:23 -0700

    Updated string functions

--- shell.h
+++ shell.h
@@ -16,5 +16,9 @@ int _strlen(const char *s);
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
 char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
 #endif /* _SHELL_H_ */
