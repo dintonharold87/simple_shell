0bfc17720c9dd0d99b3deb1f721886b92da4ba4f
DonAine <30406256+DonAine@users.noreply.github.com>
2022-07-28 02:39:04 -0700

    More String functions

---
+++ str_funcs2.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: If c is found - a pointer to the first occurence.
+ *         If c is not found - NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index]; index++)
+	{
+			if (s[index] == c)
+				return (s + index);
+	}
+
+	return (NULL);
+}
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ *         consist only of bytes from accept.
+ */
+int _strspn(char *s, char *accept)
+{
+		int bytes = 0;
+		int index;
+
+		while (*s)
+		{
+				for (index = 0; accept[index]; index++)
+				{
+						if (*s == accept[index])
+						{
+							bytes++;
+							break;
+						}
+				}
+				s++;
+		}
+		return (bytes);
+}
+
+
+
