375a7d9101be631a1fb4a0357011e6c08874e1d2
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-07-28 12:00:48 +0300

    Added string prototypes in header file

--- shell.h
+++ shell.h
@@ -1,5 +1,20 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
 
+
+/* String functions */
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
 
 #endif /* _SHELL_H_ */
