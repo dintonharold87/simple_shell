7f6d5acf75fc0ad290f8a7e1dd87bbe51b0b9e45
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-07-28 16:19:50 +0300

    A function Adds a node to the end of a alias_t linked list.

---
+++ linkedlist.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * add_alias_end - Adds a node to the end of a alias_t linked list.
+ * @head: A pointer to the head of the list_t list.
+ * @name: The name of the new alias to be added.
+ * @value: The value of the new alias to be added.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
+alias_t *add_alias_end(alias_t **head, char *name, char *value)
+{
+	alias_t *new_node = malloc(sizeof(alias_t));
+	alias_t *last;
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (!new_node->name)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = value;
+	_strcpy(new_node->name, name);
+
+	if (*head)
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
+}
+
