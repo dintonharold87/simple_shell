c65607a59358623116f00a5c97f0ff5c257a70c7
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-07-28 16:22:20 +0300

    A function that Frees a alias_t linked list.

--- linkedlist.c
+++ linkedlist.c
@@ -71,3 +71,21 @@ list_t *add_node_end(list_t **head, char *dir)
 
 	return (new_node);
 }
+
+/**
+ * free_alias_list - Frees a alias_t linked list.
+ * @head: THe head of the alias_t list.
+ */
+void free_alias_list(alias_t *head)
+{
+	alias_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head->name);
+		free(head->value);
+		free(head);
+		head = next;
+	}
+}
