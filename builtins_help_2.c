6e16b6f5fb033a65f8c87b2866dee890cb63739c
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-08-01 19:06:14 +0300

    A function that displays information about setenv function

--- builtins_help_2.c
+++ builtins_help_2.c
@@ -1,11 +1,25 @@
 #include "shell.h"
 
 /**
- * help_env - Displays information on the shellby builtin command 'env'.
+ * help_env - Displays information on the shell builtin command 'env'.
  */
 void help_env(void)
 {
 	char *msg = "env: env\n\tPrints the current environment.\n";
 
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * help_setenv - Displays information on the shell builtin command 'setenv'.
+ */
+void help_setenv(void)
+{
+	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environment variable, or modifies an existing one.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tUpon failure, prints a message to stderr.\n";
 	write(STDOUT_FILENO, msg, _strlen(msg));
 }
